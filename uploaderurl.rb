717282a4c82956c86440c6ae17ec1b1a293148b7
GilbertStawny <gilbert.stawny@lacework.net>
2022-06-16 11:03:10 -0400

    initial push

---
+++ uploaderurl.rb
@@ -0,0 +1,33 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Uploaderurl < Formula
+  desc "Script to generate a pre-signed S3 URL for customers to upload log files and diagnostic evidence."
+  homepage ""
+  url "https://github.com/GilbertStawny/uploaderURL/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "f027d49be015bd31565fbe48be6cc3324134d06f1d6c0bd567035162bd844e9f"
+  license "GPL-3.0"
+
+  # depends_on "cmake" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
+    system "./configure", *std_configure_args, "--disable-silent-rules"
+    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test uploaderURL`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
